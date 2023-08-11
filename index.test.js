5700631afa0d0e931ebb5998399ec0aeff18d5ad
Nafeesah Bari <nafeesahbari@nafeesasmacbook.myfiosgateway.com>
2023-08-11 17:26:08 -0400

    6.2 workshop

--- index.test.js
+++ index.test.js
@@ -12,11 +12,136 @@ const seedMusician = require("./seedData");
 
 describe('./musicians endpoint', () => {
     // Write your tests here
-    
-    
+    beforeAll(async () => {
+        // Seed your database before running tests
+        await db.sync({ force: true }); // This will recreate the database
+        await seedMusician(); // Seed the musicians data
+    });
 
+    afterAll(async () => {
+        await db.close(); // Close the database connection after all tests
+    });
 
+    test("GET /musicians should return a list of musicians", async () => {
+        const response = await request(app).get("/musicians");
 
+        // Verify the status code
+        expect(response.statusCode).toBe(200);
 
+        // Verify the response format
+        expect(response.type).toBe("application/json");
+
+        // Verify the response body contains an array of musicians
+        expect(Array.isArray(response.body)).toBe(true);
+    });
+
+    test("GET /musicians should return the correct number of musicians", async () => {
+        const response = await request(app).get("/musicians");
+        const musiciansFromDB = await Musician.findAll();
+
+        // Verify that the number of musicians in the response matches the number in the database
+        expect(response.body.length).toBe(musiciansFromDB.length);
+    });
+
+    test("GET /musicians should return musicians with correct properties", async () => {
+        const response = await request(app).get("/musicians");
+        const firstMusician = response.body[0];
+
+        // Verify that the musician object has the expected properties
+        expect(firstMusician).toHaveProperty("id");
+        expect(firstMusician).toHaveProperty("name");
+        expect(firstMusician).toHaveProperty("instrument");
+        // Add more property checks as needed
+    });
+
+    test("GET /musicians/:id should return a single musician", async () => {
+        const musiciansFromDB = await Musician.findAll();
+        const randomIndex = Math.floor(Math.random() * musiciansFromDB.length);
+        const randomMusician = musiciansFromDB[randomIndex];
+
+        const response = await request(app).get(`/musicians/${randomMusician.id}`);
+
+        // Verify the status code
+        expect(response.statusCode).toBe(200);
+
+        // Verify the response format
+        expect(response.type).toBe("application/json");
+
+        // Verify the response body matches the expected musician
+        expect(response.body).toEqual(expect.objectContaining({
+            id: randomMusician.id,
+            name: randomMusician.name,
+            instrument: randomMusician.instrument
+            // Add more property checks as needed
+        }));
+    });
+
+    test("GET /musicians/:id should return 404 for non-existent musician", async () => {
+        const nonExistentId = 9999; // Assuming this ID doesn't exist
+
+        const response = await request(app).get(`/musicians/${nonExistentId}`);
+
+        // Verify the status code
+        expect(response.statusCode).toBe(404);
+    });
+
+    test("POST /musicians should create a new musician", async () => {
+        const newMusicianData = {
+            name: "New Musician",
+            instrument: "Guitar"
+        };
+
+        const response = await request(app)
+            .post("/musicians")
+            .send(newMusicianData);
+
+        // Verify the status code
+        expect(response.statusCode).toBe(200);
+
+        // Verify the response format
+        expect(response.type).toBe("application/json");
+
+        // Verify the response body contains the created musician
+        expect(response.body).toMatchObject(newMusicianData);
+    });
+
+    test("PUT /musicians/:id should update an existing musician", async () => {
+        const musiciansFromDB = await Musician.findAll();
+        const randomIndex = Math.floor(Math.random() * musiciansFromDB.length);
+        const randomMusician = musiciansFromDB[randomIndex];
+
+        const updatedMusicianData = {
+            name: "Updated Musician",
+            instrument: "Piano"
+        };
+
+        const response = await request(app)
+            .put(`/musicians/${randomMusician.id}`)
+            .send(updatedMusicianData);
+
+        // Verify the status code
+        expect(response.statusCode).toBe(200);
+
+        // Verify the response format
+        expect(response.type).toBe("application/json");
+
+        // Verify the response body matches the updated musician
+        expect(response.body).toMatchObject(updatedMusicianData);
+    });
+
+    test("DELETE /musicians/:id should delete an existing musician", async () => {
+        const musiciansFromDB = await Musician.findAll();
+        const randomIndex = Math.floor(Math.random() * musiciansFromDB.length);
+        const randomMusician = musiciansFromDB[randomIndex];
+
+        const response = await request(app).delete(`/musicians/${randomMusician.id}`);
+
+        // Verify the status code
+        expect(response.statusCode).toBe(204);
+
+        // Verify that the musician is deleted
+        const deletedMusician = await Musician.findByPk(randomMusician.id);
+        expect(deletedMusician).toBeNull();
+    });
     
 })
--- src/app.js
+++ src/app.js
@@ -5,11 +5,90 @@ const { db } = require("../db/connection")
 
 const port = 3000;
 
+app.use(express.json()); // Parse JSON request bodies
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded request bodies
+
 //TODO: Create a GET /musicians route to return all musicians 
 
+app.get("/musicians", async (req, res) => {
+  try {
+    const musicians = await Musician.findAll();
+    res.json(musicians);
+  } catch (error) {
+    console.error("Error fetching musicians:", error);
+    res.status(500).json({ error: "An error occurred while fetching musicians" });
+  }
+});
 
+// New /musicians/:id route
+app.get("/musicians/:id", async (req, res) => {
+    const musicianId = req.params.id;
+    try {
+      const musician = await Musician.findByPk(musicianId);
+  
+      if (!musician) {
+        return res.status(404).json({ error: "Musician not found" });
+      }
+      res.json(musician);
+    } catch (error) {
+      console.error("Error fetching musician:", error);
+      res.status(500).json({ error: "An error occurred while fetching musician" });
+    }
+  });  
 
+// New /musicians route (POST)
+app.post("/musicians", async (req, res) => {
+    try {
+        const { name, instrument } = req.body;
+        const newMusician = await Musician.create({ name, instrument });
+        res.json(newMusician);
+    } catch (error) {
+        console.error("Error creating musician:", error);
+        res.status(500).json({ error: "An error occurred while creating musician" });
+    }
+});
 
+// Update existing musician (PUT)
+app.put("/musicians/:id", async (req, res) => {
+    const musicianId = req.params.id;
+    try {
+        const { name, instrument } = req.body;
+        const [updatedRowCount, updatedMusicians] = await Musician.update(
+            { name, instrument },
+            { where: { id: musicianId }, returning: true }
+        );
+        
+        if (updatedRowCount === 0) {
+            return res.status(404).json({ error: "Musician not found" });
+        }
+        
+        res.json(updatedMusicians[0]);
+    } catch (error) {
+        console.error("Error updating musician:", error);
+        res.status(500).json({ error: "An error occurred while updating musician" });
+    }
+});
+
+// Delete musician (DELETE)
+app.delete("/musicians/:id", async (req, res) => {
+    const musicianId = req.params.id;
+    try {
+        const deletedRowCount = await Musician.destroy({ where: { id: musicianId } });
+        
+        if (deletedRowCount === 0) {
+            return res.status(404).json({ error: "Musician not found" });
+        }
+        
+        res.status(204).send();
+    } catch (error) {
+        console.error("Error deleting musician:", error);
+        res.status(500).json({ error: "An error occurred while deleting musician" });
+    }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
 
 
